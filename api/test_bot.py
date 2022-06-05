c0bfeee8e301dc98d1aeca4ed1987337271fcfd1
raccoony <raccoonyy@gmail.com>
2022-06-05 23:12:43 +0900

    테스트 메서드를 클래스 형태로 리팩터링

--- api/test_bot.py
+++ api/test_bot.py
@@ -1,4 +1,5 @@
 import json
+from unittest import TestCase
 from unittest.mock import patch
 
 from fastapi.testclient import TestClient
@@ -15,18 +16,23 @@ def test_get_current_weather():
     assert isinstance(current_weather, CurrentWeather)
 
 
-@patch('api.bot.get_weathers')
-def test_summary(mock_api):
-    mock_api.return_value = json.loads(get_mock_response())
-    response = client.get('/summary')
-    assert response.status_code == 400
+class TestSummary(TestCase):
+    @patch('api.bot.get_weathers')
+    def test_summary_errors(self, mock_api):
+        mock_api.return_value = json.loads(get_mock_response())
+        response = client.get('/summary')
+        assert response.status_code == 400
 
-    response = client.get('/summary', params={'lat': 91, 'lon': 180})
-    assert response.status_code == 400
+        response = client.get('/summary', params={'lat': 91, 'lon': 180})
+        assert response.status_code == 400
 
-    response = client.get('/summary', params={'lat': 90, 'lon': 181})
-    assert response.status_code == 400
+        response = client.get('/summary', params={'lat': 90, 'lon': 181})
+        assert response.status_code == 400
 
-    response = client.get('/summary', params={'lat': 37.5, 'lon': 127})
-    assert response.status_code == 200
-    assert response.json()['summary']['greeting'] != ''
+    @patch('api.bot.get_weathers')
+    def test_summary(self, mock_api):
+        mock_api.return_value = json.loads(get_mock_response())
+
+        response = client.get('/summary', params={'lat': 37.5, 'lon': 127})
+        assert response.status_code == 200
+        assert response.json()['summary']['greeting'] != ''
