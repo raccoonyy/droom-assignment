2af47f893daf710b8a2bd46f270460ad27fcf6b3
raccoony <raccoonyy@gmail.com>
2022-06-05 17:12:19 +0900

    summary API 만들고 greeting 메시지 추가

--- api/bot.py
+++ api/bot.py
@@ -1,9 +1,43 @@
+import json
+import random
+from datetime import datetime, timedelta
+
 from fastapi import FastAPI
 
+from api.gen_messages import gen_greeting
+from api.models import CurrentWeather
 
 app = FastAPI()
+ONE_MINUTE = 60
 
 
-@app.get("/")
-async def root():
-    return {"Hello": "World!"}
+@app.get("/summary")
+async def summary(lat: float, lon: float):
+    weathers = await get_weathers(lat=lat, lon=lon)
+    current_weather = get_current_weather(weathers)
+
+    return {
+        "summary": {
+            "greeting": gen_greeting(current_weather),
+        }
+    }
+
+
+def get_current_weather(data):
+    now = datetime.now()
+    return [
+        CurrentWeather(**datum) for datum in data
+        if -ONE_MINUTE < int(now.timestamp()) - datum['timestamp'] < ONE_MINUTE
+    ][0]
+
+
+async def get_weathers(lat: float, lon: float):
+    return json.loads(get_mock_response())
+
+
+def get_mock_response():
+    now = datetime.now()
+
+    results = [{"timestamp": (now.timestamp()), "code": 2, "temp": 38, "rain1h": 22}]
+
+    return json.dumps(results)
---
+++ api/test_bot.py
@@ -0,0 +1,23 @@
+import json
+from unittest.mock import patch
+
+from fastapi.testclient import TestClient
+
+from api.bot import app, get_mock_response, get_current_weather
+from api.models import CurrentWeather
+
+client = TestClient(app)
+
+
+def test_get_current_weather():
+    current_weather = get_current_weather(json.loads(get_mock_response()))
+
+    assert isinstance(current_weather, CurrentWeather)
+
+
+@patch('api.bot.get_weathers')
+def test_summary(mock_api):
+    mock_api.return_value = json.loads(get_mock_response())
+    response = client.get('/summary', params={'lat': 37.5, 'lon': 127})
+    assert response.status_code == 200
+    assert response.json()['summary']['greeting'] != ''
