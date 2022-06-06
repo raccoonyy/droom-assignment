91860e01ec3bd434b37c7895525f6e2ccc06ae0f
raccoony <raccoonyy@gmail.com>
2022-06-06 11:02:50 +0900

    홑따옴표를 겹따옴표로 통일

--- api/test_bot.py
+++ api/test_bot.py
@@ -11,24 +11,24 @@ client = TestClient(app)
 
 
 class TestSummary(TestCase):
-    @patch('api.bot.get_weathers')
+    @patch("api.bot.get_weathers")
     def test_summary_errors(self, mock_api):
         mock_api.return_value = json.loads(get_mock_response())
-        response = client.get('/summary')
+        response = client.get("/summary")
         assert response.status_code == 400
 
-        response = client.get('/summary', params={'lat': 91, 'lon': 180})
+        response = client.get("/summary", params={"lat": 91, "lon": 180})
         assert response.status_code == 400
 
-        response = client.get('/summary', params={'lat': 90, 'lon': 181})
+        response = client.get("/summary", params={"lat": 90, "lon": 181})
         assert response.status_code == 400
 
-    @patch('api.bot.get_weathers')
+    @patch("api.bot.get_weathers")
     def test_summary(self, mock_api):
         mock_api.return_value = json.loads(get_mock_response())
 
-        response = client.get('/summary', params={'lat': 37.5, 'lon': 127})
+        response = client.get("/summary", params={"lat": 37.5, "lon": 127})
         assert response.status_code == 200
-        assert response.json()['summary']['greeting'] != ''
-        assert response.json()['summary']['temperature'] != ''
-        assert response.json()['summary']['heads-up'] != ''
+        assert response.json()["summary"]["greeting"] != ""
+        assert response.json()["summary"]["temperature"] != ""
+        assert response.json()["summary"]["heads-up"] != ""
